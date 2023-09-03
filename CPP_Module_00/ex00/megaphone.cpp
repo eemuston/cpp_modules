213a4970979f48fc665505235df7995b2801788e
eemuston <eemuston@student.hive.fi>
2023-09-03 17:16:46 +0300

    Learning C++. ex00 done

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (i < argc)
+	{
+		while (argv[i][j])
+		{
+			std::cout << (char)std::toupper(argv[i][j]);
+			j++;
+		}
+		j = 0;
+		std::cout << " ";
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
