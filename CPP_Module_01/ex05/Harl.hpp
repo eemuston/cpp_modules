efd6d5dcf0f057bf0b2e432483b52e74223655f8
eemuston <eemuston@student.hive.fi>
2023-10-26 05:32:08 +0300

    ex05, Don't forget to add brackets fdsjkfj

---
+++ CPP_Module_01/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 04:56:57 by eemuston          #+#    #+#             */
+/*   Updated: 2023/10/26 05:12:36 by eemuston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	public:
+		Harl(void);
+		~Harl(void);
+		void complain(std::string level);
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+};
+
+#endif
---
+++ CPP_Module_01/ex05/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 04:55:41 by eemuston          #+#    #+#             */
+/*   Updated: 2023/10/26 05:31:27 by eemuston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+}
