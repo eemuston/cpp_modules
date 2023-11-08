4adadffe2f91e14fb33366650c9e53daf7b18dc6
eemuston <eemuston@student.hive.fi>
2023-11-08 14:06:03 +0200

    very yes

--- CPP_Module_02/ex01/Fixed.cpp
+++ CPP_Module_02/ex01/Fixed.cpp
@@ -5,8 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/06 14:36:43 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/06 14:36:44 by eemuston         ###   ########.fr       */
+/*   Created: 2023/11/06 11:19:38 by eemuston          #+#    #+#             */
+/*   Updated: 2023/11/06 11:20:17 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _fixedPointValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+Fixed	&Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	if (this != &rhs)
+		this->_fixedPointValue = rhs.getRawBits();
+	return (*this);
+}
+
+int		Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_fixedPointValue = raw;
+}
--- CPP_Module_02/ex01/Fixed.hpp
+++ CPP_Module_02/ex01/Fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/06 14:35:55 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/06 14:36:33 by eemuston         ###   ########.fr       */
+/*   Created: 2023/11/06 11:16:56 by eemuston          #+#    #+#             */
+/*   Updated: 2023/11/06 11:19:28 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,17 @@
 class Fixed
 {
 	public:
+		Fixed(void);
+		Fixed(Fixed const &src);
+		~Fixed(void);
 
+		Fixed	&operator=(Fixed const &rhs);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
 	private:
-
+		int					_fixedPointValue;
+		static const int	_fractionalBits = 8;
 };
 
 #endif
