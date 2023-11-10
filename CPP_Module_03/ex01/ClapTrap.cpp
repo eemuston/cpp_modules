58f3a520723b38fd00e4d8b63d86f9ba670c571d
eemuston <eemuston@student.hive.fi>
2023-11-10 15:02:28 +0200

    starting ex01

---
+++ CPP_Module_03/ex01/ClapTrap.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 12:02:04 by eemuston          #+#    #+#             */
+/*   Updated: 2023/11/10 13:09:43 by eemuston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) : _name(name)
+{
+	std::cout << "ClapTrap constructor called" << std::endl;
+	return ;
+}
+
+ClapTrap::ClapTrap(ClapTrap const &src)
+{
+	std::cout << "ClapTrap copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+	return ;
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!" << std::endl;
+	return ;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
+	return ;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "ClapTrap " << this->_name << " is repaired " << amount << " points of damage!" << std::endl;
+	return ;
+}
---
+++ CPP_Module_03/ex01/ScavTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 13:40:58 by eemuston          #+#    #+#             */
+/*   Updated: 2023/11/10 13:45:09 by eemuston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+
+class ScavTrap
+{
+	public:
+	
+	private:
+	
+};
+
+#endif
