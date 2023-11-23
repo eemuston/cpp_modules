e936e770e5df0f511ed0869b0296d4f1f2a1bfe9
eemuston <eemuston@student.hive.fi>
2023-11-23 14:31:02 +0200

    Starting ex01

---
+++ CPP_Module_04/ex01/Animal.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 05:04:06 by eemuston          #+#    #+#             */
+/*   Updated: 2023/11/22 09:03:43 by eemuston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+	protected:
+		std::string type;
+	public:
+		Animal(void);
+		Animal(Animal const &src);
+		virtual ~Animal(void);
+		Animal &operator=(Animal const &rhs);
+		std::string getType(void) const;
+		virtual void makeSound(void) const;
+};
+
+#endif
---
+++ CPP_Module_04/ex01/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 10:11:03 by eemuston          #+#    #+#             */
+/*   Updated: 2023/11/23 12:39:44 by eemuston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
+
+int main(void) {
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	const WrongAnimal* wrong = new WrongCat();
+
+	std::cout << std::endl;
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+
+	std::cout << std::endl;
+
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	std::cout << std::endl;
+
+	std::cout << wrong->getType() << " " << std::endl;
+	wrong->makeSound();
+	
+	std::cout << std::endl;
+	
+	delete meta;
+	delete j;
+	delete i;
+	delete wrong;
+
+	return (0);
+}
