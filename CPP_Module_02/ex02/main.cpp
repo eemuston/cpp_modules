7613b2a4b3fd6f98901d0bd44a9cf8162df7c315
eemuston <eemuston@student.hive.fi>
2023-11-23 13:05:57 +0200

    updating main

--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:35:06 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/09 11:58:49 by eemuston         ###   ########.fr       */
+/*   Updated: 2023/11/09 12:13:18 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,24 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const c(10);
 	
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
+	
 	std::cout << b << std::endl;
+	
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+	
+	std::cout << c << std::endl;
+	
 	return 0;
 }
