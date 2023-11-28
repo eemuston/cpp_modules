878c066579e5753b920318de8bbc0f7c0678513f
eemuston <eemuston@student.hive.fi>
2023-11-28 03:29:29 +0200

    trying to do brain

--- CPP_Module_04/ex01/Brain.cpp
+++ CPP_Module_04/ex01/Brain.cpp
@@ -6,7 +6,39 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:30:05 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/23 14:30:06 by eemuston         ###   ########.fr       */
+/*   Updated: 2023/11/28 03:20:01 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Brain.hpp"
+
+Brain::Brain(void)
+{
+	std::cout << "Brain constructor called" << std::endl;
+}
+
+Brain::~Brain(void)
+{
+	std::cout << "Brain destructor called" << std::endl;
+}
+
+Brain::Brain(Brain const &src) 
+{
+	std::cout << "Brain copy constructor called" << std::endl;
+	*this = src;
+}
+
+Brain &Brain::operator=(Brain const &rhs)
+{
+	std::cout << "Brain assignation operator called" << std::endl;
+	if (this != &rhs) {
+		for (int i = 0; i < 100; i++)
+			this->ideas[i] = rhs.ideas[i];
+	}
+	return (*this);
+}
+
+std::string	*Brain::getIdeas(void)
+{
+	return (this->ideas);
+}
--- CPP_Module_04/ex01/Brain.hpp
+++ CPP_Module_04/ex01/Brain.hpp
@@ -6,7 +6,24 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:30:13 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/23 14:30:14 by eemuston         ###   ########.fr       */
+/*   Updated: 2023/11/28 03:20:04 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+#include <iostream>
+
+class Brain {
+	public:
+		Brain(void);
+		~Brain(void);
+		Brain(Brain const &src);
+		Brain &operator=(Brain const &rhs);
+		std::string	*getIdeas(void);
+	private:
+		std::string		ideas[100];
+};
+
+#endif
