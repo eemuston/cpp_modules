3ac18c90172ddfdb155cb22822a82efdbb40e044
eemuston <eemuston@student.hive.fi>
2023-12-02 05:49:48 +0200

    Joo o ei kuluje ei ollenkaa

--- CPP_Module_04/ex01/main.cpp
+++ CPP_Module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:11:03 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/23 12:39:44 by eemuston         ###   ########.fr       */
+/*   Updated: 2023/12/02 05:49:19 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,7 @@
 #include "Cat.hpp"
 #include "WrongCat.hpp"
 
-int main(void) {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	const WrongAnimal* wrong = new WrongCat();
-
-	std::cout << std::endl;
-
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-
-	std::cout << std::endl;
-
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-
-	std::cout << std::endl;
-
-	std::cout << wrong->getType() << " " << std::endl;
-	wrong->makeSound();
+int main(void)
+{
 	
-	std::cout << std::endl;
-	
-	delete meta;
-	delete j;
-	delete i;
-	delete wrong;
-
-	return (0);
 }
