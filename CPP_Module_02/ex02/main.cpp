36448cca8062e89f43f6b9b24e1372c0daabce4a
eemuston <eemuston@student.hive.fi>
2023-12-08 13:21:10 +0200

    Adding tests to main

--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:35:06 by eemuston          #+#    #+#             */
-/*   Updated: 2023/11/09 12:13:18 by eemuston         ###   ########.fr       */
+/*   Updated: 2023/12/08 13:17:21 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,10 @@ int main( void )
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	Fixed const c(10);
+	Fixed d(2);
+	Fixed e(5);
 	
+	std::cout << "Increments and decrements!" << std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
@@ -41,7 +44,21 @@ int main( void )
 	std::cout << a-- << std::endl;
 	std::cout << a << std::endl;
 	
-	std::cout << b << std::endl;
+	std::cout << "Multiplying and dividing!" << std::endl;
+	
+	std::cout << (d * e) << std::endl;
+	std::cout << (e / d) << std::endl;
+
+	std::cout << "Comparisons!" << std::endl;
+	
+	std::cout << (a < b) << std::endl;
+	std::cout << (a > b) << std::endl;
+	std::cout << (a <= b) << std::endl;
+	std::cout << (a >= b) << std::endl;
+	std::cout << (a == b) << std::endl;
+	std::cout << (a != b) << std::endl;
+	
+	std::cout << "Min and max!" << std::endl;
 	
 	std::cout << Fixed::max( a, b ) << std::endl;
 	std::cout << Fixed::min( a, b ) << std::endl;
