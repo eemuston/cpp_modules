3660c6d471d74afe2784706ee14b0975b6a02bcd
eemuston <eemuston@student.hive.fi>
2024-02-05 12:56:14 +0200

    Upgrades to ex00

--- CPP_Module_05/ex00/Bureaucrat.cpp
+++ CPP_Module_05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 10:45:52 by eemuston          #+#    #+#             */
-/*   Updated: 2024/01/16 12:12:25 by eemuston         ###   ########.fr       */
+/*   Updated: 2024/02/05 12:55:33 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,17 @@ Bureaucrat::Bureaucrat(std::string const name, int grade) : _name(name), _grade(
 	}
 }
 
+Bureaucrat::Bureaucrat(Bureaucrat const &src) : _name(src._name), _grade(src._grade)
+{
+	*this = src;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &src)
+{
+	_grade = src._grade;
+	return(*this);
+}
+
 Bureaucrat::~Bureaucrat()
 {
 
@@ -44,4 +55,38 @@ std::string const Bureaucrat::getName(void)
 int Bureaucrat::getGrade(void)
 {
 	return(_grade);
+}
+
+void Bureaucrat::incrementGrade(void)
+{
+	_grade--;
+	try
+	{
+		if (_grade < 1)
+			throw GradeTooHighException();
+	}
+	catch (GradeTooHighException)
+	{
+		std::cout << "Grade of the bureaucrat is too high" << std::endl;
+	}
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+	_grade++;
+	try
+	{
+		if (_grade > 150)
+			throw GradeTooLowException();
+	}
+	catch (GradeTooLowException)
+	{
+		std::cout << "Grade of the bureaucrat is too low" << std::endl;
+	}
+}
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &src)
+{
+	o << src.getName() << ", bureaucrat grade " << src.getGrade() << std::endl;
+	return(o);
 }
--- CPP_Module_05/ex00/Bureaucrat.hpp
+++ CPP_Module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 10:46:09 by eemuston          #+#    #+#             */
-/*   Updated: 2024/01/16 12:14:48 by eemuston         ###   ########.fr       */
+/*   Updated: 2024/02/05 12:55:26 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,14 @@ class Bureaucrat
 		int _grade;
 	public:
 		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(Bureaucrat const &src);
+		Bureaucrat &operator=(Bureaucrat const &src);
 		~Bureaucrat();
 		
 		std::string const getName(void);
 		int getGrade(void);
+		void incrementGrade(void);
+		void decrementGrade(void);
 		
 		class GradeTooHighException : public std::exception
 		{
@@ -36,5 +40,7 @@ class Bureaucrat
 
 		};
 };
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &src);
 
 #endif
