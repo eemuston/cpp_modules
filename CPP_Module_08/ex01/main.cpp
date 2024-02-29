4f1b8229a9369eb5818b4fe708695877cbb6c562
eemuston <eemuston@student.hive.fi>
2024-02-29 12:35:24 +0200

    Updating main a bit

--- CPP_Module_08/ex01/main.cpp
+++ CPP_Module_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:04:06 by eemuston          #+#    #+#             */
-/*   Updated: 2024/02/29 12:24:25 by eemuston         ###   ########.fr       */
+/*   Updated: 2024/02/29 12:35:14 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 int main()
 {
-	Span list(100);
-
 	try{
-		list.addNumber(100000000);
-		list.addNumber(200000000);
-		list.addNumber(300000000);
-		list.addNumber(400000000);
-		list.addNumber(500000000);
-		list.addNumber(600000000);
-		list.addNumber(1000000000);
-		list.addNumber(1000000000);
+		Span list(8);
+		list.addNumber(143);
+		list.addNumber(45);
+		list.addNumber(33);
+		list.addNumber(43);
+		list.addNumber(502);
+		list.addNumber(60);
+		list.addNumber(1543);
+		list.addNumber(342);
 		
 		int shortestSpan;
 		int longestSpan;
@@ -34,6 +33,30 @@ int main()
 
 		std::cout << "Shortestspan: " << shortestSpan << std::endl;
 		std::cout << "Longestspan: " << longestSpan << std::endl;
+		list.addNumber(2);
+	}
+	catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+	try{
+		Span list(8);
+		
+		int shortestSpan;
+		int longestSpan;
+
+		shortestSpan = list.shortestSpan();
+		longestSpan = list.longestSpan();
+
+		std::cout << "Shortestspan: " << shortestSpan << std::endl;
+		std::cout << "Longestspan: " << longestSpan << std::endl;
+	}
+	catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+	try{
+		//testofaddnumbers
 	}
 	catch(const std::exception& e)
     {
